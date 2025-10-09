3bf962a9588d8c9bd609996927602ea23f6a52f0
tegvaran <tegvaran328@gmail.com>
2025-10-09 13:44:20 -0700

    Added observer to content.js

--- scripts/content.js
+++ scripts/content.js
@@ -19,8 +19,28 @@ function renderReadingTime(article) {
 
     const date = article.querySelector("time")?.parentNode;
 
-    (data ?? heading).insertAdjacentElement("afterend", badge);
+    (date ?? heading).insertAdjacentElement("afterend", badge);
 
 }
 
 renderReadingTime(document.querySelector("article"));
+
+
+const observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+        // If a new article was added.
+        for (const node of mutation.addedNodes) {
+            if (node instanceof Element && node.tagName === "ARTICLE") {
+                // Render the reading time for this new article.
+            }
+        }
+    }
+});
+
+// https://developer.chrome.com/ is a SPA (Single Page Application) so can
+// update the address bar and render new content without reloading. Our content
+// script won't be reinjected when this happens, so we need to watch for
+// changes to the content.
+observer.observe(document.querySelector('devsite-content'), {
+  childList: true
+});
