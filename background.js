b914a688aec367172f9e2f32833f6b4f1c8fd4c5
tegvaran <tegvaran328@gmail.com>
2025-10-13 13:14:28 -0700

    Added code to change state of badge in bacground.js

--- background.js
+++ background.js
@@ -1,5 +1,28 @@
+const extensions = "https://developer.chrome.com/docs/extensions";
+const webstore = "https://developer.chrome.come/docs/webstore"
+
+
+// Adds the text "OFF" (Could be anything here) on the chome extension
+// in the dashboard above.
 chrome.runtime.onInstalled.addListener(() => {
     chrome.action.setBadgeText({
         text: "OFF",
     });
+});
+
+chrome.action.OnClicked.addListener( async (tab) => {
+    if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
+        
+        // Get the action button badge to check if the extension is 'ON' or 'OFF'
+        const prevState = await chrome.action.getBadgeText({tabId: tab.id});
+        
+        // Next state after click will be the opposite of what the current state is.
+        const nextState = prevState === 'ON' ? 'OFF' : 'ON' // if prevSate is ON set it to 'OFF' else set to 'ON' 
+        
+        // set the action badge to the next state
+        await chrome.action.setBadgeText({
+            tabId: tab.id,
+            text: nextState,
+        });
+    }
 });
