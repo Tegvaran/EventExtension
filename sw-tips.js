c9e74fe5476af9357f54c99798d44b1875299cea
tegvaran <tegvaran328@gmail.com>
2025-10-17 17:56:11 -0700

    Added alarm code for tip service worker in sw-tips

--- sw-tips.js
+++ sw-tips.js
@@ -1,1 +1,26 @@
-console.log('sw-tips.js');
+// Fetch a tip and save it in storage.
+const updateTip = async () => {
+    const response = await fetch('https://chrome.dev/f/extension_tips/');
+    const tips = await response.json();
+    const randomIndex = Math.floor(Math.random() * tips.length);
+    return chrome.storage.local.set({tip: tips[randomIndex]});
+};
+
+const ALARM_NAME = 'tip';
+
+async function createAlarm() {
+    const alarm = await chrome.alarms.get(ALARM_NAME);
+
+    if (typeof alarm === 'undefined') {
+        chrome.alarms.create(ALARM_NAME, {
+            delayInMinutes: 1,
+            periodInMinutes: 1440
+        });
+        updateTip();
+    }
+}
+
+createAlarm();
+
+// Update tip once a day 
+chrome.alarms.onAlarm.addListner(updateTip);
